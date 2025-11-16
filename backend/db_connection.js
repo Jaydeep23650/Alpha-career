72edb93466c0837c67b804b14848940a13fcf62e
Jaydeep23650 <jaydeepjnvmzp2002@gmail.com>
2025-11-16 14:38:48 +0530

    add base url

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-const connectionString = "mongodb+srv://jaydeepjnvmzp2002_db_user:hESNwTIRBwzYlQag@cluster0.0by1msm.mongodb.net/?appName=Cluster0"||"mongodb://localhost:27017/job_portal";
+//  const connectionString = "mongodb+srv://jaydeepjnvmzp2002_db_user:hESNwTIRBwzYlQag@cluster0.0by1msm.mongodb.net/?appName=Cluster0"||"mongodb://localhost:27017/job_portal";
+
+const connectionString = process.env.MONGODB_URI || "mongodb+srv://jaydeepjnvmzp2002_db_user:hESNwTIRBwzYlQag@cluster0.0by1msm.mongodb.net/?appName=Cluster0";
 const dbConnection = async () => {
     try {
         await mongoose.connect(connectionString);
--- job/config/api.ts
+++ job/config/api.ts
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'https://alpha-career.onrender.com';
+export const API_BASE_URL = 'https://alpha-career-bakcend.onrender.com';
 export const getApiUrl = (endpoint: string): string => {
   const cleanEndpoint = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
   return `${API_BASE_URL}${cleanEndpoint}`;
